eca9133a6b2cbde0a56c14f3e5674a68881780d8
jawadahmed5 <jawadahmed.xwave5@gmail.com>
2025-01-28 16:01:37 +0500

    wekend offers products

--- src/Components/StaysPage/HomePage/HeroSection/HeroSection.jsx
+++ src/Components/StaysPage/HomePage/HeroSection/HeroSection.jsx
@@ -31,6 +31,19 @@ import TurkeySeven from "../../../../assets/images/istangbol-city-seven.jpg"
 import TurkeyEight from "../../../../assets/images/istangbol-city-eight.jpg"
 import TurkeyNine from "../../../../assets/images/istangbol-city-ten.jpg"
 import TurkeyTen from "../../../../assets/images/istangbol-city-nine.jpg"
+import Deal1 from "../../../../assets/images/Deal-5.jpg"
+import Deal2 from "../../../../assets/images/Deal-6.jpg"
+import Deal3 from "../../../../assets/images/Deal-7.jpg"
+import Deal4 from "../../../../assets/images/Deal-8.jpg"
+import Deal5 from "../../../../assets/images/Deal-9.jpg"
+import Deal7 from "../../../../assets/images/Deal-11.jpg"
+import Deal8 from "../../../../assets/images/Deal-12.jpg"
+import Deal9 from "../../../../assets/images/Deal-13.jpg"
+import Deal10 from "../../../../assets/images/Deal-1.jpg"
+import Deal11 from "../../../../assets/images/Deal-2.jpg"
+import Deal12 from "../../../../assets/images/Deal-3.jpg"
+import Deal13 from "../../../../assets/images/Deal-4.jpg"
+
 
 const HeroSection = () => {
   const [city, setCity] = useState("");
@@ -72,18 +85,190 @@ const HeroSection = () => {
 
 
   const cities = [
-    { name: "Istanbul", properties: "4256 properties", img: { TurkeyOne } },
-    { name: "Cappadocia", properties: "685 properties", img: "https://example.com/cappadocia.jpg" },
-    { name: "Antalya", properties: "604 properties", img: "https://example.com/antalya.jpg" },
-    { name: "Göreme", properties: "206 properties", img: "https://example.com/goreme.jpg" },
-    { name: "Ankara", properties: "296 properties", img: "https://example.com/ankara.jpg" },
-    { name: "Bodrum", properties: "544 properties", img: "https://example.com/bodrum.jpg" },
-    { name: "İzmir", properties: "272 properties", img: "https://example.com/izmir.jpg" },
-    { name: "Trabzon", properties: "835 properties", img: "https://example.com/trabzon.jpg" },
-    { name: "Bursa", properties: "252 properties", img: "https://example.com/bursa.jpg" },
-    { name: "Alanya", properties: "953 properties", img: "https://example.com/alanya.jpg" }
+    { name: "Istanbul", properties: "4256 properties", img: TurkeyOne },
+    { name: "Cappadocia", properties: "685 properties", img: TurkeyTwo },
+    { name: "Antalya", properties: "604 properties", img: TurkeyThree },
+    { name: "Göreme", properties: "206 properties", img: TurkeyFour },
+    { name: "Ankara", properties: "296 properties", img: TurkeyFive },
+    { name: "Bodrum", properties: "544 properties", img: TurkeySix },
+    { name: "İzmir", properties: "272 properties", img: TurkeySeven },
+    { name: "Trabzon", properties: "835 properties", img: TurkeyEight },
+    { name: "Bursa", properties: "252 properties", img: TurkeyNine },
+    { name: "Alanya", properties: "953 properties", img: TurkeyTen }
   ];
 
+  const hotels = [
+    {
+      image: Deal1,
+      buttonText: "GENIUS",
+      name: "Magnova Studios",
+      location: "Istanbul, Turkey",
+      rating: 8.9,
+      reviews: "Excellent · 756 reviews",
+      deal: "Early 2025 Deal",
+      nights: 2,
+      originalPrice: 124538,
+      discountedPrice: 53552,
+    },
+    {
+      image: Deal2,
+      buttonText: "GENIUS",
+      name: "GOODWOOD SUITES HOMES VACATION",
+      location: "Dubai, United Arab Emirates",
+      rating: 9.1,
+      reviews: "Wonderful · 192 reviews",
+      deal: "Early 2025 Deal",
+      nights: 2,
+      originalPrice: 197973,
+      discountedPrice: 126109,
+    },
+    {
+      image: Deal3,
+      buttonText: "GENIUS",
+      name: "Nawazi Towers Hotel",
+      location: "Mecca, Saudi Arabia",
+      rating: 8.4,
+      reviews: "Very Good · 2,558 reviews",
+      nights: 2,
+      originalPrice: 60013,
+      discountedPrice: 39008,
+    },
+    {
+      image: Deal4,
+      buttonText: "GENIUS",
+      name: "Hotel Erguvan - Special Category",
+      location: "Istanbul, Turkey",
+      rating: 8.6,
+      reviews: "Excellent · 344 reviews",
+      nights: 2,
+      originalPrice: 35808,
+      discountedPrice: 27214,
+    },
+    {
+      image: Deal5,
+      buttonText: "GENIUS",
+      name: "Riviera Hotel",
+      location: "Dubai, United Arab Emirates",
+      rating: 8.5,
+      reviews: "Very Good · 1,396 reviews",
+      deal: "Early 2025 Deal",
+      nights: 2,
+      originalPrice: 75852,
+      discountedPrice: 59164,
+    },
+    {
+      image: Deal7,
+      buttonText: "GENIUS",
+      name: "فندق بياك أوتيل الروضة",
+      location: "Mecca, Saudi Arabia",
+      rating: 8.3,
+      reviews: "Very Good · 2,918 reviews",
+      nights: 2,
+      originalPrice: 16192,
+      discountedPrice: 13763,
+    },
+    {
+      image: Deal8,
+      buttonText: "GENIUS",
+      name: "Dosso Dossi Hotels & SPA Golden Horn",
+      location: "Istanbul, Turkey",
+      rating: 8.7,
+      reviews: "Excellent · 3,410 reviews",
+      deal: "Limited-time Deal",
+      nights: 2,
+      originalPrice: 58067,
+      discountedPrice: 30774,
+    },
+    {
+      image: Deal9,
+      buttonText: "GENIUS",
+      name: "City Seasons Towers Hotel Bur Dubai",
+      location: "Dubai, United Arab Emirates",
+      rating: 7.9,
+      reviews: "Good · 6,509 reviews",
+      deal: "Early 2025 Deal",
+      nights: 2,
+      originalPrice: 55842,
+      discountedPrice: 41882,
+    },
+    {
+      image: Deal10,
+      buttonText: "GENIUS",
+      name: "Diouf Al Maqam Hotel 4",
+      location: "Mecca, Saudi Arabia",
+      rating: 8.2,
+      reviews: "Very Good · 1,424 reviews",
+      nights: 2,
+      originalPrice: 34504,
+      discountedPrice: 13802,
+    },
+    {
+      image: Deal11,
+      buttonText: "GENIUS",
+      name: "Misa Hotel",
+      location: "Istanbul, Turkey",
+      rating: 8.3,
+      reviews: "Very Good · 858 reviews",
+      deal: "Limited-time Deal",
+      nights: 2,
+      originalPrice: 66811,
+      discountedPrice: 24720,
+    },
+    {
+      image: Deal12,
+      buttonText: "GENIUS",
+      name: "Holiday Inn Express Dubai Airport, an IHG Hotel",
+      location: "Dubai, United Arab Emirates",
+      rating: 8.2,
+      reviews: "Very Good · 21,872 reviews",
+      deal: "Early 2025 Deal",
+      nights: 2,
+      originalPrice: 66598,
+      discountedPrice: 49948,
+    },
+    {
+      image: Deal13,
+      buttonText: "GENIUS",
+      name: "فندق ضيوف المقام 3 للغرف والشقق المفروشة توصيل مجاني للحرم",
+      location: "Mecca, Saudi Arabia",
+      rating: 8.3,
+      reviews: "Very Good · 2,466 reviews",
+      nights: 2,
+      originalPrice: 34504,
+      discountedPrice: 13802,
+    },
+  ];
+
+
+
+  const destinations = [
+    {
+      image: "los-angeles-hotel.jpg",
+      title: "5 of the best hotels in Los Angeles",
+      subtitle: "From Hollywood to Beverly Hills discover 5 of the best hotels in Los Angeles for your stay",
+    },
+    {
+      image: "orlando-hotels.jpg",
+      title: "The 6 best Orlando hotels for families",
+      subtitle: "Discover the best Orlando hotels for families for your vacation.",
+    },
+    {
+      image: "ski-towns.jpg",
+      title: "5 best ski towns around the world",
+      subtitle: "Discover a winter wonderland in these charming ski destinations",
+    },
+    {
+      image: "thanksgiving-vacation-homes.jpg",
+      title: "5 vacation homes for a Thanksgiving getaway",
+      subtitle: "Enjoy Thanksgiving dinner at these vacation homes.",
+    },
+    {
+      image: "bangkok-rooftop-bars.jpg",
+      title: "6 incredible Bangkok rooftop bars",
+      subtitle: "Amazing city views, cocktails, and world-class cuisine.",
+    },
+  ];
+  
 
 
 
@@ -335,7 +520,7 @@ const HeroSection = () => {
 
 
 
-{/* 
+    {/* 
     <Box className="container mt-5">
       <Typography className="fw-bold fs-4">Explore Turkey</Typography>
       <Box
@@ -370,63 +555,211 @@ const HeroSection = () => {
 
 
     <Box className='container' >
-  <Typography>
-    Explore Turkey
-  </Typography>
+      <Typography className='fs-5 fw-bold mt-4'>
+        Explore Turkey
+      </Typography>
 
-  <Box
-    sx={{
-      display: { xs: 'flex', sm: 'flex', md: 'flex', lg: 'flex' }, // Show carousel only on small screens
-      gap: 2,
-      padding: '10px',
-      paddingBottom: '15px',
-      paddingTop: '15px',
-      alignItems: 'center',
-      overflowX: 'auto', // Enables horizontal scrolling
-      width: '100%',
-      // backgroundColor: '#0D6EFD',
-      scrollSnapType: 'x mandatory',
-      '& > *': {
-        scrollSnapAlign: 'center',
-        display: 'flex', // Ensure items are in line for horizontal scrolling
-        marginRight: 0, // Spacing between items
-      },
-      '::-webkit-scrollbar': { display: 'none' }, // Hide scrollbar
-    }}
-  >
-    <Box
-      sx={{
-        display: 'flex',
-        gap: 2,
-        flexWrap: 'nowrap', // Prevents wrapping
-        overflowX: 'auto', // Enables scrolling
-        width: '100%',
-      }}
-    >
-      {cities.slice(0, 10).map((city) => (
-        <div
-          style={{
-            border: '1px solid #ddd',
-            padding: 10,
-            margin: 10,
-            borderRadius: 8,
-            textAlign: 'center',
-            flex: '0 0 auto', // Prevent items from growing or shrinking
+      <Box
+        sx={{
+
+          display: "flex",
+          gap: 2,
+          padding: '0px',
+          paddingBottom: '15px',
+          paddingTop: '10px',
+          alignItems: 'center',
+          overflowX: 'auto',
+          width: '100%',
+          scrollSnapType: 'x mandatory',
+          '& > *': {
+            scrollSnapAlign: 'center',
+
+
+          },
+          '::-webkit-scrollbar': { display: 'none' }, // Hide scrollbar
+        }}
+
+
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            gap: 2,
+            flexWrap: 'nowrap',
+            overflowX: 'auto',
+            width: '100%',
+
+            '::-webkit-scrollbar': { display: 'none' },
+
           }}
-          key={city.name}
+
         >
-          <img
-            src={city.img}
-            alt={city.name}
-            style={{ width: '100%', maxWidth: 150, minWidth: 150, borderRadius: 8 }}
-          />
-          <h3>{city.name}</h3>
-          <p>{city.properties}</p>
-        </div>
-      ))}
+          {cities.slice(0, 10).map((city) => (
+            <Box
+              style={{
+                border: '1px solid #ddd',
+                padding: 0,
+                margin: 0,
+                borderRadius: 8,
+                textAlign: 'center',
+                flex: '0 0 auto',
+                border: "none"
+              }}
+
+              key={city.name}
+            >
+
+              <img
+                src={city.img}
+                alt={city.img}
+                style={{ width: '100%', maxWidth: 140, minWidth: 140, borderRadius: 8 }}
+              />
+
+
+              <Box>
+
+                <Typography className='fs-6 fw-bold text-start mt-1' >{city.name}</Typography>
+                <Typography className='text-start' style={{ fontSize: "12px" }}>{city.properties}</Typography>
+              </Box>
+
+            </Box>
+
+
+          ))}
+        </Box>
+      </Box>
     </Box>
-  </Box>
-</Box>
+
+
+
+    <Box className="container">
+      {/* Section Title */}
+      <Typography className="fs-5 fw-bold mt-4">Deals for the weekend</Typography>
+
+      {/* Scrollable Carousel */}
+      <Box
+        sx={{
+          display: "flex",
+          gap: 2,
+          paddingTop: "10px",
+          paddingBottom: "15px",
+          overflowX: "auto",
+          scrollSnapType: "x mandatory",
+          "& > *": {
+            scrollSnapAlign: "center",
+          },
+          "::-webkit-scrollbar": { display: "none" }, // Hide scrollbar
+        }}
+      >
+        {hotels.map((hotel, index) => (
+          <Box
+
+            className=' rounded-2'
+            key={index}
+            sx={{
+              // border: "1px solid #ddd",
+              boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+              borderRadius: 2,
+              flex: "0 0 auto",
+              minWidth: "220px",
+              maxWidth: "220px",
+
+            }}
+          >
+            {/* Hotel Image */}
+            <Box
+              component="img"
+              src={hotel.image}
+              alt={hotel.name}
+              sx={{
+                width: "100%",
+                height: "155px",
+                objectFit: "cover",
+                borderRadius: "8px",
+              }}
+            />
+
+            {/* Button Tag */}
+            <Button
+              className='text-start mt-1 ms-1 text-white'
+              variant="contained"
+              size="small"
+              sx={{
+                backgroundColor: "#0071c2",
+                fontSize: "8px",
+              }}
+            >
+              {hotel.buttonText}
+            </Button>
+
+            {/* Hotel Details */}
+            <Box className='mt-1 text-start ms-1'>
+              <Typography
+
+                style={{ fontSize: "13px" }} className="fw-bolder"
+              >
+                {hotel.name}
+              </Typography>
+              <Typography style={{ fontSize: "10px" }} className="fw-lighter" color="textSecondary">
+                {hotel.location}
+              </Typography>
+
+              <Typography className='d-flex'>
+
+
+                <Typography width={20} className='bg-primary text-white px-1 pt-1 fw-bold rounded-top rounded-end text-center' style={{ fontSize: "10px" }} variant="body2">
+                  {hotel.rating}
+                </Typography>
+                <Typography style={{ fontSize: "10px" }} className="fw-lighter ms-2 d-flex justify-content-center align-items-center">{hotel.reviews}</Typography>
+
+              </Typography>
+
+            </Box>
+
+            {/* Pricing Details */}
+            <Box className='mt-1 text-start ms-1'>
+              {hotel.deal && (
+                <Typography
+                  style={{ fontSize: "9px" }}
+                  className='bg-success text-white p-1 rounded-1 fw-light'
+                  variant="caption"
+                  color="success.main"
+                  fontWeight="bold"
+                >
+                  {hotel.deal}
+                </Typography>
+              )}
+
+
+            </Box>
+            <Box className='d-flex justify-content-end  ' >
+
+              <Typography
+                style={{ fontSize: "9px" }}
+
+              >
+                {hotel.nights} nights for{" "}
+                <del
+                  style={{ fontSize: "12px" }}
+
+                  className='text-danger '>Rs. {hotel.originalPrice.toLocaleString()}</del>
+              </Typography>
+
+              <Typography
+
+                style={{ fontSize: "12px" }}
+                className='fw-bold text-dark ms-2 align-bottom align-text-bottom     ' color="primary">
+                PKR {hotel.discountedPrice.toLocaleString()}
+              </Typography>
+            </Box>
+
+          </Box>
+        ))}
+      </Box>
+    </Box>
+
+
+
 
 
   </>
